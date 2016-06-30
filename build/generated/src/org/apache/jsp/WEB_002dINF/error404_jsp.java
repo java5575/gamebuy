e50278610639ab204a4bf0a028f79a4d7559fa07
caesar <newman0934@gmail.com>
2016-06-30 19:17:58 +0800

    2016/6/30 update
    2016/6/30 update

---
+++ build/generated/src/org/apache/jsp/WEB_002dINF/error404_jsp.java
@@ -0,0 +1,103 @@
+package org.apache.jsp.WEB_002dINF;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class error404_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    Throwable exception = org.apache.jasper.runtime.JspRuntimeLibrary.getThrowable(request);
+    if (exception != null) {
+      response.setStatus((Integer)request.getAttribute("javax.servlet.error.status_code"));
+    }
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html;charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\n");
+      out.write("<!DOCTYPE html>\n");
+      out.write("<html>\n");
+      out.write("    <head>\n");
+      out.write("        <title>404錯誤</title>\n");
+      out.write("        <meta charset=\"UTF-8\">\n");
+      out.write("        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n");
+      out.write("        <style type=\"text/css\">\n");
+      out.write("            body {\n");
+      out.write("                background-image: url(");
+      out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${pageContext.request.contextPath}", java.lang.String.class, (PageContext)_jspx_page_context, null));
+      out.write("/images/ERROR404.jpg);\n");
+      out.write("                background-size: cover;\n");
+      out.write("\n");
+      out.write("            }\n");
+      out.write("            p{\n");
+      out.write("                font-family: Microsoft JhengHei;\n");
+      out.write("                display: block;\n");
+      out.write("                position: relative;\n");
+      out.write("                width: 400px;\n");
+      out.write("                margin: 0 auto;\n");
+      out.write("                top: 550px;\n");
+      out.write("              \n");
+      out.write("            }\n");
+      out.write("            a{\n");
+      out.write("                color: #ffffff;\n");
+      out.write("                font-size: 50px;\n");
+      out.write("                font-weight: bold;\n");
+      out.write("                text-decoration: none;\n");
+      out.write("            }\n");
+      out.write("            a:hover{\n");
+      out.write("                color:#ff0000;\n");
+      out.write("            }\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("        </style>\n");
+      out.write("    </head>\n");
+      out.write("    <body>\n");
+      out.write("        <p><a href=\"");
+      out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${pageContext.request.contextPath}", java.lang.String.class, (PageContext)_jspx_page_context, null));
+      out.write("/index.jsp\">請點此回到首頁</a></p>\n");
+      out.write("    </body>\n");
+      out.write("</html>");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
